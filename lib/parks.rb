9bb7b7dd6d7ea40e9f1a1fb388a607cfe6ac7322
carolgreene <carolgreene3@gmail.com>
2020-01-27 04:06:51 -0800

    required lib files

--- lib/parks.rb
+++ lib/parks.rb
@@ -1,5 +1,8 @@
-require "parks/version"
+require_relative "./parks/version"
 
-module Parks
-  # Your code goes here...
-end
+#module Parks
+  # Your code goes here...  ##Don't need this here. Module is already in version.rb
+#end
+
+#Load the rest of the lib files
+require_relative "./parks/cli"  
