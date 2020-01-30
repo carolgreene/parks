0911cebd3d3849ed6f1a861341e0ea9983cc9a56
carolgreene <carolgreene3@gmail.com>
2020-01-29 18:16:21 -0800

    moved scraping out of Park to it's own class

--- lib/parks/park.rb
+++ lib/parks/park.rb
@@ -6,20 +6,6 @@ class Parks::Park
     self.scrape_parks
   end
 
-  def self.scrape_parks 
-    
-    #scrape parks from web- will adjust later to make them objects instead of strings
-    #info listed below for parks & locations give correct tags to scrape for those items
-    doc = Nokogiri::HTML(open('https://www.timeout.com/chicago/things-to-do/the-best-parks-in-chicago'))
-    #parks = doc.css('header.xs-mb2').collect do |park|
-     # park.css('h3').text.gsub(/\s+/," ").strip
-    #end 
-    #locations = doc.css('header.xs-mb2').collect do |loc|
-      #loc.css('span').text.gsub(/\s+/," ").strip
-    #end
-    
-    binding.pry
-    parks
-
-  end
 end
+    
+    
