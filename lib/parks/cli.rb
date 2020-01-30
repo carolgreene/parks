397fb281df23f7845aeaaad6334de30fb2f8748d
carolgreene <carolgreene3@gmail.com>
2020-01-29 17:53:32 -0800

    changed menu to if statement & modified list_parks

--- lib/parks/cli.rb
+++ lib/parks/cli.rb
@@ -8,9 +8,14 @@ class Parks::CLI
     end
 
     def list_parks
-      puts "1. Millennium Park"
-      puts "2. Grant Park"
-      puts "3. The 606"
+      #puts "1. Millennium Park:  Millennium Park"
+      #puts "2. Grant Park:  Millennium Park"
+      #puts "3. The 606:  Humboldt Park"
+      @parks = Parks::Park.all
+      
+      @parks.each.with_index(1) do |park, i|
+        puts "#{i}. #{park.name} - #{park.location}"
+      end
     end
 
     def menu      
@@ -18,15 +23,13 @@ class Parks::CLI
       while input != "exit" 
         puts "enter the number of the park you'd like more info on or type list to see parks again or type exit"
         input = gets.strip.downcase
-        case input
-        when "1"
-          puts "More info on park 1 ..."
-        when "2"
-          puts "More info on park 2..."
-        when "3"
-          puts "More info on park 3..."
-        when "list"
+
+        if input.to_i > 0
+          puts @parks[input.to_i-1].name
+        elsif input == "list"
           list_parks
+        else
+          puts "not sure what you want, type list or exit"
         end
       end
     end
