2645d41e93d679194d0d3abf0a1a16c0a3fcd636
carolgreene <carolgreene3@gmail.com>
2020-01-30 15:06:00 -0800

    added call for scraper method & adjusted code layout

--- lib/parks/cli.rb
+++ lib/parks/cli.rb
@@ -1,41 +1,38 @@
 class Parks::CLI
 
-    def call
-      puts "Best Parks in Chicago"
-      list_parks
-      menu
-      goodbye
-    end
-
-    def list_parks
-      #puts "1. Millennium Park:  Millennium Park"
-      #puts "2. Grant Park:  Millennium Park"
-      #puts "3. The 606:  Humboldt Park"
-      @parks = Parks::Park.all
-      
-      @parks.each.with_index(1) do |park, i|
-        puts "#{i}. #{park.name} - #{park.location}"
-      end
-    end
-
-    def menu      
-      input = nil
-      while input != "exit" 
-        puts "enter the number of the park you'd like more info on or type list to see parks again or type exit"
-        input = gets.strip.downcase
-
-        if input.to_i > 0
-          puts @parks[input.to_i-1].name
-        elsif input == "list"
-          list_parks
-        else
-          puts "not sure what you want, type list or exit"
-        end
-      end
-    end
-
-    def goodbye
-      puts "see you later"
-    end
-  
+  def call
+    puts "Best Parks in Chicago"
+    Parks::Scraper.scrape_parks
+    list_parks
+    menu
+    goodbye
   end
+
+  def list_parks
+    @parks = Parks::Park.all      
+    @parks.each.with_index(1) do |park, i|
+      puts "#{i}. #{park.name} - #{park.location}"
+    end
+  end
+
+  def menu      
+    input = nil
+    while input != "exit" 
+      puts "enter the number of the park you'd like more info on or type list to see parks again or type exit"
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        puts @parks[input.to_i-1].name 
+      elsif input == "list"
+        list_parks
+      else
+        puts "not sure what you want, type list or exit"
+      end
+    end
+  end
+
+  def goodbye
+    puts "see you later"
+  end
+
+end
