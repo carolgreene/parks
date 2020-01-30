4839d68cd8f3cbc8496c0131aa0400b254133373
carolgreene <carolgreene3@gmail.com>
2020-01-29 18:30:04 -0800

    added basic methods for Park class

--- lib/parks/park.rb
+++ lib/parks/park.rb
@@ -1,10 +1,27 @@
 class Parks::Park
-    attr_accessor :name, :location
+  attr_accessor :name, :location
+
+  @@all = []
+
+  def initialize(name = nil, location = nil, description = nil)
+    @name = name
+    @location = location
+    @description = description
+    save
+  end
+
+  def save 
+    @@all << self
+  end
 
   def self.all
-    #need to scrape for actual data
-    self.scrape_parks
+    @@all 
   end
+
+  def self.find(id)
+    self.all[id-1]
+  end
+  
 
 end
     
