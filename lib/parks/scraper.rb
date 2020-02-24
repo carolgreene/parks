870904cd6ec285085f57afd961d633224aab5b9c
carolgreene <carolgreene3@gmail.com>
2020-02-23 17:01:27 -0800

    removed prys & cleaned up code

--- lib/parks/scraper.rb
+++ lib/parks/scraper.rb
@@ -4,18 +4,15 @@ class Parks::Scraper
     doc = Nokogiri::HTML(open('https://www.timeout.com/chicago/things-to-do/the-best-parks-in-chicago'))
     doc.css('header.xs-mb2').each do |info|
       park = Parks::Park.new
-      #binding.pry
       park.name = info.search('h3').text.gsub(/\s+/," ").strip
       park.location = info.search('span').text.gsub(/\s+/," ").strip
       park.url = info.search("a").first["href"]
-      binding.pry
     end
   end
 
   def self.scrape_description(park)
-    #binding.pry
     detail = Nokogiri::HTML(open('https://www.timeout.com/chicago/attractions/millennium-park-chicago-il'))
+    park.summary = detail.search("span", itemprop = "reviewBody" "p" "strong").text.gsub(/\s+/," ").strip
   end
-        
-
+  
 end
