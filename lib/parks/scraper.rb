0aa7672ea369da49bc9bb85f81d39686e53f3c02
carolgreene <carolgreene3@gmail.com>
2020-02-23 18:52:23 -0800

    fixed url & scraping for description

--- lib/parks/scraper.rb
+++ lib/parks/scraper.rb
@@ -6,13 +6,16 @@ class Parks::Scraper
       park = Parks::Park.new
       park.name = info.search('h3').text.gsub(/\s+/," ").strip
       park.location = info.search('span').text.gsub(/\s+/," ").strip
-      park.url = info.search("a").first["href"]
+      park.url = "https://www.timeout.com" + info.search("a").first["href"]
+      
     end
   end
 
   def self.scrape_description(park)
-    detail = Nokogiri::HTML(open('https://www.timeout.com/chicago/attractions/millennium-park-chicago-il'))
+    #binding.pry
+    detail = Nokogiri::HTML(open(park.url))
     park.summary = detail.search("span", itemprop = "reviewBody" "p" "strong").text.gsub(/\s+/," ").strip
+    #binding.pry
   end
-  
+
 end
