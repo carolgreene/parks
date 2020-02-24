58dad5c5ba90756194a7b6be229da2a2ebcb7291
carolgreene <carolgreene3@gmail.com>
2020-02-23 16:59:07 -0800

    added choice to menu & rearranged if statement

--- lib/parks/cli.rb
+++ lib/parks/cli.rb
@@ -21,14 +21,25 @@ class Parks::CLI
       puts "enter the number of the park you'd like more info on or type list to see parks again or type exit"
       input = gets.strip.downcase
 
-      if input.to_i > 0
-        puts @parks[input.to_i-1].name 
-      elsif input == "list"
+      if input == "list"
         list_parks
+      elsif input == "exit"
+        goodbye
+      elsif input.to_i > 0 
+        choice = Parks::Park.find(input.to_i)
+        #binding.pry
+        puts '' 
+        puts "You chose number #{input}. #{choice.name}."
+        puts "Here are some details"
+        print_park(choice)
       else
         puts "not sure what you want, type list or exit"
       end
     end
+  end
+
+  def print_park(choice)
+    puts "you're in print park"
   end
 
   def goodbye
