1e2b4287b67c62d4d39eefe2a292e5cf86b0a2f7
carolgreene <carolgreene3@gmail.com>
2020-02-23 18:53:20 -0800

    added park details & description

--- lib/parks/cli.rb
+++ lib/parks/cli.rb
@@ -23,8 +23,6 @@ class Parks::CLI
 
       if input == "list"
         list_parks
-      elsif input == "exit"
-        goodbye
       elsif input.to_i > 0 
         choice = Parks::Park.find(input.to_i)
         #binding.pry
@@ -32,14 +30,36 @@ class Parks::CLI
         puts "You chose number #{input}. #{choice.name}."
         puts "Here are some details"
         print_park(choice)
+
+        puts "Would you like to find out more? Enter 'Yes' or 'No' (Y/N)."
+        more_info = gets.strip.upcase
+        until more_info == "Y" || more_info == "YES" || more_info == "N" || more_info == "NO"
+          puts "That's not a valid answer, please enter 'Yes' or 'No' (Y/N)."
+          more_info = gets.strip.upcase
+        end
+        if more_info == "Y" || more_info == "YES"
+          Parks::Scraper.scrape_description(Parks::Park.find(input.to_i))
+          print_description(choice)
+        end
+
       else
         puts "not sure what you want, type list or exit"
       end
+      puts " "
+    
     end
   end
 
   def print_park(choice)
-    puts "you're in print park"
+    puts "#{choice.name}"
+    puts "#{choice.location}"
+    puts "#{choice.url}"    
+  end
+
+  def print_description(choice)
+    puts "#{choice.summary}"
+    puts ""
+    puts ""
   end
 
   def goodbye
